9d6ad15d511a90c6568b61446ce285c2422cf3fd
gufran-ops <gufran022december@kgpian.iitkgp.ac.in>
2021-06-14 00:23:54 +0545

    Features/code-added
    Finished the game

---
+++ NSSAppForClass(1-2)/Assets/Gufran/Scripts/PanelsSpawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelsSpawn: MonoBehaviour
+{
+    public GameObject[] Panels;
+    public int rnd;
+    int i;
+    public void random()
+    {
+        rnd = Random.Range(1, 10);
+       // Debug.Log("Spawn");
+        //Debug.Log(rnd);
+        for ( i=0; i<rnd; i++)
+        {
+            Panels[i].SetActive(true);
+        }
+        
+    }
+    void Start()
+    {
+        random();
+    }
+
+    
+}
