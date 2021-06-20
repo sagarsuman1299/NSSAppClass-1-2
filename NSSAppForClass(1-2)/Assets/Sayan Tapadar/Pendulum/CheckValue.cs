162b68ed754d3348dc1995ee9ac2533ed6f755d2
Syntrpd <85694635+Syntrpd@users.noreply.github.com>
2021-06-20 20:10:08 +0530

    Feature/Pendulum
    Added pendulum question to count total no. of oscillations of pendulum.

---
+++ NSSAppForClass(1-2)/Assets/Sayan Tapadar/Pendulum/CheckValue.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class CheckValue : MonoBehaviour
+{
+    public Pendulum pp;
+    public TextMeshProUGUI tmp;
+
+    string ans;
+
+    private void Start()
+    {
+        tmp.text = "";
+    }
+
+    public void check(string str)
+    {
+        if (str == ((pp.total + 1) + ""))
+        {
+            Debug.Log("yayy");
+            tmp.text = "";
+            ans= "CORRECT";
+            StartCoroutine("showRes");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        else
+        {
+            tmp.text = "";
+            ans= "INCORRECT";
+            StartCoroutine("showRes");
+            Debug.Log("no");
+        }
+    }
+
+    IEnumerator showRes()
+    {
+        tmp.text = ans;
+        yield return new WaitForSeconds(2);
+        tmp.text = "";
+    }
+}
