c8eea4887662baf1a32165a05d4e33bb838210c2
Sagar Suman <sagarsuman1299@gmail.com>
2021-11-25 21:46:30 +0530

    final

---
+++ NSSAppForClass(1-2)/Assets/Sagar/WebLink.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebLink : MonoBehaviour
+{
+    public void SagarLinkdn()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/sagar-suman-66974b199/");
+    }
+
+    public void SagarFb()
+    {
+        Application.OpenURL("https://www.facebook.com/sagarsuman1299");
+    }
+    public void DJLinkdn()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/divyajyoti-nayak-3619731a1");
+    }
+
+    public void DJFb()
+    {
+        Application.OpenURL("https://www.facebook.com/profile.php?id=100009215008158");
+    }
+    public void AkshatLn()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/akshat-jain-221305220/");
+    }
+    public void AkshatFb()
+    {
+        Application.OpenURL("https://www.facebook.com/profile.php?id=100055623349710");
+    }
+    public void PrateekLn()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/prateek-dhaka-381a73207/");
+    }
+    public void PrateekFb()
+    {
+        Application.OpenURL("https://www.facebook.com/profile.php?id=100008420526354");
+    }
+
+    public void SatyajeetLn()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/satyajit-sahu-625a51211");
+    }
+    public void SombitFb()
+    {
+        Application.OpenURL("https://www.facebook.com/search/top?q=iit%20kharagpur");
+    }
+    public void SombitLn()
+    {
+        Application.OpenURL("https://www.facebook.com/search/top?q=iit%20kharagpur");
+    }
+    public void SatyajeetFb()
+    {
+        Application.OpenURL("https://www.facebook.com/satyajit.sahu.771282");
+    }
+    public void MayankLn()
+    {
+        Application.OpenURL("https://www.facebook.com/search/top?q=iit%20kharagpur");
+    }
+    public void MayankFb()
+    {
+        Application.OpenURL("https://www.facebook.com/search/top?q=iit%20kharagpur");
+    }
+    public void GufranLn()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/mohd-gufran-ansari");
+    }
+    public void GufranFb()
+    {
+        Application.OpenURL("https://www.facebook.com/mohdgufran.ansari.7771");
+    }
+    public void AniketLn()
+    {
+        Application.OpenURL("http://www.linkedin.com/in/aniket-pan");
+    }
+    public void AniketFb()
+    {
+        Application.OpenURL("https://www.facebook.com/aniket.pan.72/");
+    }
+    public void RudrakLn()
+    {
+        Application.OpenURL("https://www.facebook.com/search/top?q=iit%20kharagpur");
+    }
+    public void RudrakFb()
+    {
+        Application.OpenURL("https://www.facebook.com/search/top?q=iit%20kharagpur");
+    }
+    public void AribLn()
+    {
+        Application.OpenURL("https://www.facebook.com/search/top?q=iit%20kharagpur");
+    }
+    public void AribFb()
+    {
+        Application.OpenURL("https://www.facebook.com/search/top?q=iit%20kharagpur");
+    }
+
+
+}
